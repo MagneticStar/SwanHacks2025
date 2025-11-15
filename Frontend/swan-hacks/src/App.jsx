bd417074a6f026ddcf5e9dca06bec216f6a2771f
MagneticStar <jobanowetz@icloud.com>
2025-11-15 17:51:14 -0600

    feat: partial commit of home page structure

--- Frontend/swan-hacks/src/App.jsx
+++ Frontend/swan-hacks/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 
 import UserSignup from './components/user/UserSignup';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
       <div className="flex-grow">
         <Routes>
           <Route path="/signup" element={<UserSignup user={user} setUser={setUser} />} />
+          <Route path="/" element={<Home/>} />       
         </Routes>
       </div>
     </Router>
